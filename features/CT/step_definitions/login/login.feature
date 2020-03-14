c9996f4234bff8beb818518f40dbdcdfff80d0ff
Bruno Sales <bruno.pgm@outlook.com>
2020-03-14 16:01:50 -0300

    Inicio do projeto

---
+++ features/CT/step_definitions/login/login.feature
@@ -0,0 +1,12 @@
+#language:pt
+
+Funcionalidade: Fazer login
+Eu como usuario do site
+Quero realizar o login
+Para ser logado no site
+
+@login
+Cenario: Realizar login
+Dado que eu esteja na home do site
+Quando eu preencher e-mail, senha e apertar no botao login
+Entao eu logo
---
+++ features/CT/step_definitions/login/login.rb
@@ -0,0 +1,11 @@
+Dado("que eu esteja na home do site") do
+    visit 'http://automationpractice.com/index.php'
+  end
+  
+  Quando("eu preencher e-mail, senha e apertar no botao login") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Entao("eu logo") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
